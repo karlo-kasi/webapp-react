d2f6efbf27d01befaa15bd066d177504ff7e9322
karlo-kasi <karlokasi1995@gmail.com>
2025-03-18 12:52:22 +0100

    Add: milestone 3

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,43 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+
+
 export default function HomePage() {
-    return (
-      <>
-        <h1 className="text-primary">Films</h1>
-        <h2>Qui andranno tutti i film</h2>
-        <div className="row row-cols-3">{/* Qui andranno tutti i film */}</div>
-      </>
-    );
+
+  const [movies, setMovies] = useState([]);
+
+  const fetchShow = () => {
+    axios.get("http://localhost:3000/movies")
+      .then((res) => {
+        setMovies(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
+
+  const renderMovie = () => {
+    return movies.map((movie) => {
+        return (
+          <div className="col" key={movie.id}>
+            <MovieCard movie={movie} />
+          </div>
+        )
+      })
+  }
+
+
+
+  useEffect(fetchShow, [])
+
+  return (
+    <>
+      <h1 className="text-primary">Films</h1>
+      <h2>Qui andranno tutti i film</h2>
+      <div className="row row-cols-3">
+       {renderMovie()}
+      </div>
+    </>
+  );
+}
